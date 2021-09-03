7fc79138d7aebb37b974c447ab417638e3d339ea
Nathan Ho <nathan.waikin.ho@gmail.com>
2021-09-03 16:25:26 -0700

    Add better integration tests for conversion

--- tests/test_canvas.py
+++ tests/test_canvas.py
@@ -20,59 +20,105 @@ def canvas(pytestconfig):
     return result
 
 @pytest.fixture
-def example_image():
+def stereo_image():
     image = PIL.Image.new("RGB", (100, 100))
     draw = PIL.ImageDraw.Draw(image)
-    draw.rectangle([(0, 0), (100, 100)], fill=(255, 0, 255))
+    draw.rectangle([(0, 0), (100, 100)], fill=(255, 0, 128))
     return image
 
-@pytest.fixture
-def example_sound():
-    sample_rate = 48000
-    duration = 1.0
-    frequency = 440.0
+def sine_wave(sample_rate, duration, frequency):
     duration_in_samples = int(sample_rate * duration)
     t = np.arange(duration_in_samples) / sample_rate
     sound = np.sin(2 * np.pi * t * frequency)[:, np.newaxis]
     return sound
 
+@pytest.fixture
+def mono_sound():
+    return sine_wave(48000, 1.0, 440)
+
+@pytest.fixture
+def stereo_sound():
+    return np.hstack([
+        sine_wave(48000, 1.0, 440),
+        sine_wave(48000, 1.0, 441),
+    ])
+
 def test_help(canvas):
     """canvas --help runs without error."""
     subprocess.run([canvas, "--help"], check=True)
 
-def test_image_to_image(canvas, example_image):
+def test_image_to_image(canvas, stereo_image):
     """canvas with image import and export preserves color."""
     with tempfile.TemporaryDirectory() as directory:
         root = pathlib.Path(directory)
-        example_image.save(root / "in.png")
+        stereo_image.save(root / "in.png")
         subprocess.run([canvas, "-t", "-i", root / "in.png", "-o", root / "out.png"])
         out_image = PIL.Image.open(root / "out.png")
-        assert out_image.getpixel((0, 0)) == (255, 0, 255, 255)
-        assert out_image.size[1] == 239
+        assert out_image.getpixel((0, 0))[:3] == stereo_image.getpixel((0, 0))[:3]
 
-def test_image_to_sound(canvas, example_image):
-    """Converting an image to sound produces non-silent audio."""
+def test_image_to_sound(canvas, stereo_image):
+    """Converting an image to sound produces non-silent, stereo audio with different
+    channels."""
     with tempfile.TemporaryDirectory() as directory:
         root = pathlib.Path(directory)
-        example_image.save(root / "in.png")
+        stereo_image.save(root / "in.png")
         subprocess.run([canvas, "-t", "-i", root / "in.png", "-o", root / "out.wav"])
         out_sound, __ = soundfile.read(root / "out.wav")
+
+        assert out_sound.shape[1] == 2
         assert np.any(out_sound != 0)
+        assert np.any(out_sound[:, 0] != out_sound[:, 1])
 
-def test_sound_to_image(canvas, example_sound):
-    """Converting a sound to image produces a non-blank image."""
+def test_mono_sound_to_image(canvas, mono_sound):
+    """Converting a mono sound to image produces a non-blank image with the following
+    properties:
+    - blue channel equals red channel
+    - green channel is blank
+    """
     with tempfile.TemporaryDirectory() as directory:
         root = pathlib.Path(directory)
-        soundfile.write(root / "in.wav", example_sound, 48000)
+        soundfile.write(root / "in.wav", mono_sound, 48000)
         subprocess.run([canvas, "-t", "-i", root / "in.wav", "-o", root / "out.png"])
-        out_image = PIL.Image.open(root / "out.png")
-        assert np.any(np.asarray(out_image)[:, :, :3] != 0)
+        out_image = np.asarray(PIL.Image.open(root / "out.png"))
 
-def test_sound_to_sound(canvas, example_sound):
-    """Converting sound to sound produces non-silent audio."""
+        assert np.any(out_image[:, :, :3] != 0)
+        np.testing.assert_allclose(out_image[:, :, 1], 0)
+        np.testing.assert_allclose(out_image[:, :, 0], out_image[:, :, 2])
+
+def test_stereo_sound_to_image(canvas, stereo_sound):
+    """Converting a stereo sound to image produces a non-blank image with the following
+    properties:
+    - blue channel does not equal red channel
+    - green channel is blank
+    """
     with tempfile.TemporaryDirectory() as directory:
         root = pathlib.Path(directory)
-        soundfile.write(root / "in.wav", example_sound, 48000)
+        soundfile.write(root / "in.wav", stereo_sound, 48000)
+        subprocess.run([canvas, "-t", "-i", root / "in.wav", "-o", root / "out.png"])
+        out_image = np.asarray(PIL.Image.open(root / "out.png"))
+
+        assert np.any(out_image[:, :, :3] != 0)
+        np.testing.assert_allclose(out_image[:, :, 1], 0)
+        assert np.any(out_image[:, :, 0] != out_image[:, :, 2])
+
+def test_mono_sound_to_sound(canvas, mono_sound):
+    """Converting sound to sound produces non-silent, stereo audio with both channels equal."""
+    with tempfile.TemporaryDirectory() as directory:
+        root = pathlib.Path(directory)
+        soundfile.write(root / "in.wav", mono_sound, 48000)
         subprocess.run([canvas, "-t", "-i", root / "in.wav", "-o", root / "out.wav"])
         out_sound, rate = soundfile.read(root / "out.wav")
+        assert out_sound.shape[1] == 2
         assert np.any(out_sound != 0)
+        np.testing.assert_allclose(out_sound[:, 0], out_sound[:, 1])
+
+def test_stereo_sound_to_sound(canvas, stereo_sound):
+    """Converting sound to sound produces non-silent, stereo audio with channels different."""
+    with tempfile.TemporaryDirectory() as directory:
+        root = pathlib.Path(directory)
+        soundfile.write(root / "in.wav", stereo_sound, 48000)
+        subprocess.run([canvas, "-t", "-i", root / "in.wav", "-o", root / "out.wav"])
+        out_sound, rate = soundfile.read(root / "out.wav")
+        assert out_sound.shape[1] == 2
+        assert np.any(out_sound != 0)
+        assert np.any(out_sound[:, 0] != out_sound[:, 1])
