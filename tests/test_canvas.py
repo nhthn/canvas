efe7247e919fab2444908e26a83e2a5bfd372609
Nathan Ho <nathan.waikin.ho@gmail.com>
2021-09-03 21:17:01 -0700

    Add regression test

--- tests/test_canvas.py
+++ tests/test_canvas.py
@@ -9,10 +9,12 @@ import PIL.ImageDraw
 import pytest
 import soundfile
 
+REGRESSION_TEST_ROOT = pathlib.Path(__file__).resolve().parent / "regression_tests"
+
 
 @pytest.fixture
 def canvas(pytestconfig):
-    result = pytestconfig.getoption("executable")
+    result = pytestconfig.getoption("--executable")
     if result is None:
         raise RuntimeError(
             "Missing --executable pytest option. Please point it to the canvas executable."
@@ -20,10 +22,30 @@ def canvas(pytestconfig):
     return result
 
 @pytest.fixture
-def stereo_image():
+def write_regtests(pytestconfig):
+    return pytestconfig.getoption("--write-regtests")
+
+@pytest.fixture
+def flat_image():
     image = PIL.Image.new("RGB", (100, 100))
     draw = PIL.ImageDraw.Draw(image)
     draw.rectangle([(0, 0), (100, 100)], fill=(255, 0, 128))
+    return image
+
+@pytest.fixture
+def gradient_image():
+    size = 100
+    image = PIL.Image.new("RGB", (size, size))
+    for i in range(size):
+        for j in range(size):
+            image.putpixel(
+                (i, j),
+                (
+                    int(i / size * 255),
+                    min(int((i + j) / size * 255), 255),
+                    int(j / size * 255)
+                )
+            )
     return image
 
 def sine_wave(sample_rate, duration, frequency):
@@ -47,21 +69,21 @@ def test_help(canvas):
     """canvas --help runs without error."""
     subprocess.run([canvas, "--help"], check=True)
 
-def test_image_to_image(canvas, stereo_image):
+def test_image_to_image(canvas, flat_image):
     """canvas with image import and export preserves color."""
     with tempfile.TemporaryDirectory() as directory:
         root = pathlib.Path(directory)
-        stereo_image.save(root / "in.png")
+        flat_image.save(root / "in.png")
         subprocess.run([canvas, "-t", "-i", root / "in.png", "-o", root / "out.png"], check=True)
         out_image = PIL.Image.open(root / "out.png")
-        assert out_image.getpixel((0, 0))[:3] == stereo_image.getpixel((0, 0))[:3]
+        assert out_image.getpixel((0, 0))[:3] == flat_image.getpixel((0, 0))[:3]
 
-def test_image_to_sound(canvas, stereo_image):
+def test_image_to_sound(canvas, flat_image):
     """Converting an image to sound produces non-silent, stereo audio with different
     channels."""
     with tempfile.TemporaryDirectory() as directory:
         root = pathlib.Path(directory)
-        stereo_image.save(root / "in.png")
+        flat_image.save(root / "in.png")
         subprocess.run([canvas, "-t", "-i", root / "in.png", "-o", root / "out.wav"], check=True)
         out_sound, __ = soundfile.read(root / "out.wav")
 
@@ -102,7 +124,7 @@ def test_stereo_sound_to_image(canvas, stereo_sound):
         assert np.any(out_image[:, :, 0] != out_image[:, :, 2])
 
 def test_mono_sound_to_sound(canvas, mono_sound):
-    """Converting sound to sound produces non-silent, stereo audio with both channels equal."""
+    """Converting mono sound to sound produces non-silent, stereo audio with both channels equal."""
     with tempfile.TemporaryDirectory() as directory:
         root = pathlib.Path(directory)
         soundfile.write(root / "in.wav", mono_sound, 48000)
@@ -113,7 +135,7 @@ def test_mono_sound_to_sound(canvas, mono_sound):
         np.testing.assert_allclose(out_sound[:, 0], out_sound[:, 1])
 
 def test_stereo_sound_to_sound(canvas, stereo_sound):
-    """Converting sound to sound produces non-silent, stereo audio with channels different."""
+    """Converting stereo sound to sound produces non-silent, stereo audio with channels different."""
     with tempfile.TemporaryDirectory() as directory:
         root = pathlib.Path(directory)
         soundfile.write(root / "in.wav", stereo_sound, 48000)
@@ -122,3 +144,22 @@ def test_stereo_sound_to_sound(canvas, stereo_sound):
         assert out_sound.shape[1] == 2
         assert np.any(out_sound != 0)
         assert np.any(out_sound[:, 0] != out_sound[:, 1])
+
+def test_image_to_sound_regression(canvas, gradient_image, write_regtests):
+    """Regression test for converting image to sound."""
+
+    regression_test_file = REGRESSION_TEST_ROOT / "image_to_sound.wav"
+
+    with tempfile.TemporaryDirectory() as directory:
+        root = pathlib.Path(directory)
+        gradient_image.save(root / "in.png")
+        subprocess.run([canvas, "-t", "-i", root / "in.png", "-o", root / "out.wav"], check=True)
+        sound, rate = soundfile.read(root / "out.wav")
+
+    if write_regtests:
+        soundfile.write(regression_test_file, sound, rate, subtype="FLOAT")
+
+    expected_sound, expected_rate = soundfile.read(regression_test_file)
+
+    assert rate == expected_rate
+    np.testing.assert_allclose(sound, expected_sound)
