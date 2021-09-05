19c65d4c807067159cf550167a26441fbfe8ef18
Nathan Ho <nathan.waikin.ho@gmail.com>
2021-09-04 18:34:34 -0700

    Add stub for benchmarking Synth

---
+++ src/benchmark/benchmark.cpp
@@ -0,0 +1,24 @@
+#include <benchmark/benchmark.h>
+#include "../Synth.hpp"
+
+static void benchSynthProcess(benchmark::State& state) {
+    std::mt19937 randomEngine(0);
+    Synth synth(48000, randomEngine);
+
+    for (int i = 0; i < synth.getNumOscillators(); i++) {
+        synth.setOscillatorAmplitude(i, 1, 1);
+    }
+
+    int frameCount = 64;
+    float outputBufferLeft[64];
+    float outputBufferRight[64];
+    float* outputBuffer[2] = { outputBufferLeft, outputBufferRight };
+
+    for (auto _ : state) {
+        synth.process(2, outputBuffer, frameCount);
+    }
+}
+// Register the function as a benchmark
+BENCHMARK(benchSynthProcess);
+
+BENCHMARK_MAIN();
